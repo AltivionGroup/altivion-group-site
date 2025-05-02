25c18fbe247784e447f9446c767b7189d94985b9
Altivion Group <altiviongroup@gmail.com>
2025-05-02 15:06:40 -0400

    Update jesse-lattimore.js

--- src/pages/businesscards/jesse-lattimore.js
+++ src/pages/businesscards/jesse-lattimore.js
@@ -25,25 +25,22 @@ export default function JesseLattimoreCard() {
           {/* Logos and Company Centered Above Name */}
           <div className="flex flex-col items-center gap-2 mt-4">
             {/* Altivion Logo */}
-            <Image
-              src="/logo.png"
-              alt="Altivion Logo"
-              width={60}
-              height={60}
-              className="mx-auto"
-            />
-
-            {/* Venture Logos Side-by-Side */}
-            <div className="flex justify-center gap-4 mt-2">
-              <Image
-                src="/venturelogo/waypointgeospatial-logo.png"
+             <Image
+                src="/venturelogo/wpg-rbg-logo.png"
                 alt="Waypoint Geospatial"
                 width={100}
                 height={35}
                 className="object-contain"
               />
               <Image
-                src="/venturelogo/fuelify-logo.png"
+                src="/venturelogo/fuel-rbg-logo.png"
+                alt="Fuelify"
+                width={100}
+                height={35}
+                className="object-contain"
+              />
+              <Image
+                src="/venturelogo/b8-rbg-logo.png"
                 alt="Fuelify"
                 width={100}
                 height={35}
