5f296b2a3c696e2d33b367a6868e816e16c5f266
Altivion Group <altiviongroup@gmail.com>
2025-05-02 15:02:19 -0400

    Update wes-jones.js

--- src/pages/businesscards/wes-jones.js
+++ src/pages/businesscards/wes-jones.js
@@ -26,7 +26,7 @@ export default function WesleyJonesCard() {
           <div className="flex flex-col items-center gap-2 mt-4">
             {/* Altivion Logo */}
             <Image
-              src="/logo.png"
+              src="/venturelogo/alt-rbg-logo.png"
               alt="Altivion Logo"
               width={60}
               height={60}
@@ -36,14 +36,21 @@ export default function WesleyJonesCard() {
             {/* Venture Logos Side-by-Side */}
             <div className="flex justify-center gap-4 mt-2">
               <Image
-                src="/venturelogo/waypointgeospatial-logo.png"
+                src="/venturelogo/wpg-rbg-logo.png"
                 alt="Waypoint Geospatial"
                 width={100}
                 height={35}
                 className="object-contain"
               />
               <Image
-                src="/venturelogo/fuelify-logo.png"
+                src="/venturelogo/fuel-rbg-logo.png"
+                alt="Fuelify"
+                width={100}
+                height={35}
+                className="object-contain"
+              />
+              <Image
+                src="/venturelogo/fuel-rbg-logo.png"
                 alt="Fuelify"
                 width={100}
                 height={35}
